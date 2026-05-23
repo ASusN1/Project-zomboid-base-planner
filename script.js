260214b500f4482ef1be5649e0ea80ca98cbfd8c
ASusN1 <tuele2035@gmail.com>
2026-05-23 00:54:11 -0400

    clearn up - stuck rn

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ viewContainer.addEventListener('mouseleave',() => {
 })
 
 //For the tab system 
-function openItems(evt, itemName) {
+function switchCategory(evt, itemName) {
     //declare all variables 
     var i, tabconnect, tablinks;
     // get all elelment with class contnetn and hide them
@@ -96,3 +96,21 @@ function openItems(evt, itemName) {
     document.getElementById(itemName).style.display = "block"; // tester later if this block can be repalce with just text 
     evt.currentTarget.className += " active";
 }
+// will put in docker or smt since this not suppose to be in the js code since it going to be long
+const itemDatabase = {
+    Wall :[
+        { name: 'Brick Wall', color: '#B22222' },
+        { name: 'Stone Wall', color: '#808080' },
+        { name: 'Wooden Wall', color: '#8B4513' }
+    ],
+    Floor: [
+        { name: 'Wooden Floor', color: '#DEB887' },
+        { name: 'Marble Floor', color: '#F5F5F5' },
+        { name: 'Tile Floor', color: '#D3D3D3' }
+    ],
+    Furniture: [
+        { name: 'Chair', color: '#8B4513' },
+        { name: 'Table', color: '#DEB887' }
+    ]
+}
+//----------------------------------------------------
