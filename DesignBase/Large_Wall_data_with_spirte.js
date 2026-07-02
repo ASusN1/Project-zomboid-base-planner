d7fd09a1f7012c908593766c487b2dc7694798ab
ASusN1 <tuele2035@gmail.com>
2026-07-01 21:28:27 -0400

    temp fix: draw the picture into flat using piskel

--- DesignBase/Large_Wall_data_with_spirte.js
+++ DesignBase/Large_Wall_data_with_spirte.js
@@ -4,10 +4,10 @@ const large_wall_sprite_data = [
         type: "wall",
         height: "large",
         Wall_sprite: {
-            "N": "../tile/Wall/large_Wall/Wood_Wall_level_3_NS.png",
-            "E": "../tile/Wall/large_Wall/Wood_Wall_level_3_EW.png",
-            "S": "../tile/Wall/large_Wall/Wood_Wall_level_3_NS.png", 
-            "W": "../tile/Wall/large_Wall/Wood_Wall_level_3_EW.png"
+            "N": "../tile/Wall/large_Wall/Wood_wall_level_3_draw_version_NS.png",
+            "E": "../tile/Wall/large_Wall/Wood_wall_level_3_draw_version_EW.png",
+            "S": "../tile/Wall/large_Wall/Wood_wall_level_3_draw_version_NS.png", 
+            "W": "../tile/Wall/large_Wall/Wood_wall_level_3_draw_version_EW.png"
         } 
     }
 ];
