1bde583c2e89e96b512081e2971b5006aa8afafb
ASusN1 <tuele2035@gmail.com>
2026-07-09 13:10:28 -0400

    toggle view ( switch between grid view and box view

--- Home/sidebar_switch_active.js
+++ Home/sidebar_switch_active.js
@@ -10,3 +10,10 @@ communityDesignsBtn.addEventListener("click", () => {
     communityDesignsBtn.classList.add("active");
     yourDesignsBtn.classList.remove("active");
 })
+
+const toggleViewBtn = document.getElementById('toggleViewBtn');
+const cardRow = document.getElementById('cardRow');
+
+toggleViewBtn.addEventListener("click", () => {
+    cardRow.classList.toggle("list-view");
+});
