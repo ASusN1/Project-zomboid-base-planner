a4c1fbed9945452568e734038fc638010b289131
ASusN1 <tuele2035@gmail.com>
2026-07-10 22:24:33 -0400

    just move file so it easier to locate

--- sb_stuff/supabase_client.js
+++ sb_stuff/supabase_client.js
@@ -2,8 +2,8 @@
 // pull the createcleitn function from supabase hosted library 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://fnsaseeilkbxwetczteo.supabase.co';
-const SUPABASE_ANON_KEY = "sb_publishable_N89aipq75mX6Ak6C6awIzg_EnunANRO"; 
+const SUPABASE_URL = '';
+const SUPABASE_ANON_KEY = ""; 
 
 window.sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
