475fdcc4916ab581fe9cdd957d92c50fc8b0b70e
ASusN1 <tuele2035@gmail.com>
2026-07-11 08:39:08 -0400

    download base design as png ( partially work)

---
+++ DesignBase/download_base_design.js
@@ -0,0 +1,40 @@
+async function exportBaseDesignAsPicture() { 
+    
+    document.querySelector(".tools-bar").style.display = "none"; // hide tools bar
+    document.querySelector(".base-design-name").style.display = "none"; // hide base design name
+
+    await new Promise(r => requestAnimationFrame(r)); // wait for the next frame to ensure the UI is updated
+
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: { displaySurface: "browser" },
+        preferCurrentTab: true
+    });
+
+    const video = document.createElement("video");
+    video.srcObject = stream;
+    await video.play();
+
+    await new Promise(r => requestAnimationFrame(r)); // wait for the next frame to ensure the video is playing
+
+    const canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext("2d");
+
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    stream.getTracks().forEach(track => track.stop()); // stop the stream
+
+    
+    document.querySelector(".tools-bar").style.display = ""; // show tools bar
+    document.querySelector(".base-design-name").style.display = ""; // show base design name
+
+    canvas.toBlob(blob => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "base_design.png";
+        link.click();
+    });
+}
+
+window.exportBaseDesignAsPicture = exportBaseDesignAsPicture;
