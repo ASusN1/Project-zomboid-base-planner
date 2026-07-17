19e1e27ed2b0f2f9407d4a9de6c8bb061b75edf0
ASusN1 <tuele2035@gmail.com>
2026-07-17 10:20:11 -0400

    aboutbtnswith

--- Home/sidebar_switch_active.js
+++ Home/sidebar_switch_active.js
@@ -1,9 +1,11 @@
 const yourDesignsBtn = document.getElementById('yourDesignsBtn');
 const communityDesignsBtn = document.getElementById('communityDesignsBtn');
+const aboutBtn = document.getElementById('aboutBtn');
 
 yourDesignsBtn.addEventListener("click", () => {
     yourDesignsBtn.classList.add("active");
     communityDesignsBtn.classList.remove("active");
+    aboutBtn.classList.remove("active");
     cardRow.style.display = "";
     communityCardRowContainer.style.display = "none";
 });
@@ -11,9 +13,16 @@ yourDesignsBtn.addEventListener("click", () => {
 communityDesignsBtn.addEventListener("click", () => {
     communityDesignsBtn.classList.add("active");
     yourDesignsBtn.classList.remove("active");
+    aboutBtn.classList.remove("active");
     communityCardRowContainer.style.display = "";
     cardRow.style.display = "none";
     loadCOmmunityDesignsFromSupabase(); // load community designs when button clicked
+});
+
+aboutBtn.addEventListener("click", () => {
+    communityDesignsBtn.classList.remove("active");
+    yourDesignsBtn.classList.remove("active");
+    aboutBtn.classList.add("active");
 });
 
 const toggleViewBtn = document.getElementById('toggleViewBtn');
