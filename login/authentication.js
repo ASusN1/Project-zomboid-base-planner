e0a29e226e951dbdc096d0a88ee7981358b58f19
ASusN1 <tuele2035@gmail.com>
2026-07-17 09:13:00 -0400

    workin on user name

--- login/authentication.js
+++ login/authentication.js
@@ -1,7 +1,8 @@
 const loginForm = document.getElementById('loginForm');
 const authMessage = document.getElementById('authMessage');
 const signUpButton = document.getElementById('signUpButton');
-const allowedCharacter = /^[a-zA-Z0-9_]+$/; // regex for allowed characters in username
+
+const allowedCharacter = /^[a-zA-Z0-9_]{3,15}$/; // Allow A-Z, a-z, 0-9, _, lenght between 3 and 15 characters
 
 loginForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // stop page from reloading 
@@ -28,6 +29,12 @@ loginForm.addEventListener("submit", async (event) => {
 signUpButton.addEventListener('click', async () => {
     const emailValue = document.getElementById('email').value;
     const passwordValue = document.getElementById('passwordInput').value;
+    const usernameValue = document.getElementById('usernameInput').value;
+
+    if (!allowedCharacter.test(usernameValue)) {
+        authMessage.textContent = "Invalid username. Please use only letters, numbers, and underscores, and ensure it is between 3 and 15 characters long.";
+        return;
+    }
 
     authMessage.textContent = "Creating account...";
 
@@ -40,6 +47,16 @@ signUpButton.addEventListener('click', async () => {
         authMessage.textContent = error.message;
         return; 
     }
+    const newUser = data.user;
+    if (newUser) { 
+        const profileInsertResult = await window.sb
+            .from('profiles')
+            .insert({id: newUser.id, username: usernameValue});
 
+        if(profileInsertResult.error) {
+            authMessage.textContent = profileInsertResult.error.message;
+        }
+        
+    }
     authMessage.textContent = "Account created successfully. Please check your email to confirm your account.";
 });
