ec6a0d88c78ff6dc9fd3df641000d069c850b0dc
ASusN1 <tuele2035@gmail.com>
2026-08-04 11:42:51 -0400

    update reset pass

--- resetPassword/reset_password.js
+++ resetPassword/reset_password.js
@@ -1,7 +1,54 @@
+const step_email_section = document.getElementById("step-email-section");
+const step_code_section = document.getElementById("step-code-section");
+
+const email_form = document.getElementById("email-input-form");
+const reset_email_input = document.getElementById("reset-email-input");
+const email_step_message = document.getElementById("reset-email-message");
+
+const codeForm = document.getElementById("code-input-form");
+const rest_code_input = document.getElementById("reset-code-input");
+const code_step_message = document.getElementById("code-step-message");
+const resend_code_btn = document.getElementById("resend-code-btn");
+
 const reset_password_form = document.getElementById("reset-password-form");
 const new_password_input = document.getElementById("new-password");
 const confirm_password_input = document.getElementById("confirm-password-input");
 const reset_password_message = document.getElementById("reset-password-message");
+
+
+let email_being_reset = "";
+ email_form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email_value = reset_email_input.value.trim();
+    email_being_reset = email_value;
+    email_step_message.textContent = "Sending reset code to email: " + email_value;
+
+    const sendResult = await window.sb.auth.resetPasswordForEmail(email_value);
+    const sendError = sendResult.error;
+
+    // If there's an error, display it
+    if(sendError) {
+        email_step_message.textContent = "Error sending reset code: " + sendError.message;
+        return;
+    }
+    email_step_message.textContent = "code sent successfully to email: " + email_value;
+
+    step_email_section.style.display = "none";
+    step_code_section.style.display = "";
+ });
+
+ resend_code_btn.addEventListener("click", async (event) => {
+    code_step_message.textContent = "resending reset code to email: " + email_being_reset;
+    const resendResult = await window.sb.auth.resetPasswordForEmail(email_being_reset);
+    const resendError = resendResult.error;
+
+    if(resendError){
+        code_step_message.textContent = "Error resending reset code: " + resendError.message;
+        return;
+    }
+    code_step_message.textContent = "code resent successfully to email: " + email_being_reset;
+ });
 
 reset_password_form.addEventListener("submit", async (event) => {
     event.preventDefault();
