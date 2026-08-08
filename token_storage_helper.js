d1e77c55c263b4ca16983636520084022b2d0ee9
ASusN1 <tuele2035@gmail.com>
2026-08-07 22:17:37 -0400

    fix bug

--- token_storage_helper.js
+++ token_storage_helper.js
@@ -1,9 +1,9 @@
-function saveAuthToken(accessToken, refreshToken){
+function saveAuthTokens(accessToken, refreshToken){
     localStorage.setItem("pzbp_access_token", accessToken);
     localStorage.setItem("pzbp_refresh_token", refreshToken);
 }
 
-function clearAuthToken(){
+function clearAuthTokens(){
     localStorage.removeItem("pzbp_access_token");
     localStorage.removeItem("pzbp_refresh_token");
 }
@@ -14,13 +14,13 @@ function getStoredAccessToken(){
 
 function getAuthHeader(){
     const token = getStoredAccessToken();
-    if(token){
-        return[];
+    if (!token){
+        return{};
     }
     return {Authorization: "Bearer " + token};
 }
 
-window.saveAuthToken = saveAuthToken;
-window.clearAuthToken = clearAuthToken;
+window.saveAuthTokens = saveAuthTokens;
+window.clearAuthTokens = clearAuthTokens;
 window.getStoredAccessToken = getStoredAccessToken;
 window.getAuthHeader = getAuthHeader;
